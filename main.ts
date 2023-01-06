2e4a952b8b4d840b8aa7ff8bdc5ff5e801cab726
sa5160 <122075279+sa5160@users.noreply.github.com>
2023-01-06 13:29:17 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+basic.forever(function () {
+	
+})
